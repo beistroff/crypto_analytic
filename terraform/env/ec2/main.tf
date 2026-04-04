eaab813a83e99e20f4bceaaf91394ccf8c6d72d4
beistroff <galiasandrii@gmail.com>
2026-04-04 14:58:00 +0300

    sg group rules

--- terraform/env/ec2/main.tf
+++ terraform/env/ec2/main.tf
@@ -19,14 +19,68 @@ data "aws_ami" "ubuntu_22_04" {
   }
 }
 
+data "aws_vpc" "default" {
+  default = true
+}
+
+resource "aws_security_group" "sentinel_sg" {
+  name        = "${var.instance_name}-sg"
+  description = "Security group for Sentinel EC2 instance (Outbound only)"
+  vpc_id      = data.aws_vpc.default.id
+
+  egress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "HTTPS for APIs, SSM, and secure web"
+  }
+
+  egress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "HTTP for apt updates and general web"
+  }
+
+  egress {
+    from_port   = 53
+    to_port     = 53
+    protocol    = "udp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "DNS Resolution (UDP)"
+  }
+
+  egress {
+    from_port   = 53
+    to_port     = 53
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "DNS Resolution (TCP)"
+  }
+
+  egress {
+    from_port   = 123
+    to_port     = 123
+    protocol    = "udp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "NTP for clock synchronization"
+  }
+
+  tags = {
+    Name = "${var.instance_name}-sg"
+  }
+}
+
 resource "aws_instance" "sentinel_agent" {
   ami                  = data.aws_ami.ubuntu_22_04.id
   instance_type        = var.instance_type
   iam_instance_profile = var.iam_instance_profile_name
   user_data            = local.user_data_script
+  vpc_security_group_ids = [aws_security_group.sentinel_sg.id]
 
   tags = {
     Name = var.instance_name
   }
 }
-
